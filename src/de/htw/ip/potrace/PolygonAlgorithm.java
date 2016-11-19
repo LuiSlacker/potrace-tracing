223da28e4322b608a5e5d8bd9f184b7e66848a75
Petulant Slacker <l.goohsen@gmail.com>
2016-11-19 18:19:41 +0100

    further implementation

--- src/de/htw/ip/potrace/PolygonAlgorithm.java
+++ src/de/htw/ip/potrace/PolygonAlgorithm.java
@@ -36,29 +36,33 @@ public class PolygonAlgorithm {
 		return pivots;
 	}
 
-	public static List<Point> straightPathPolygon(List<Point> contour, Point vertex) {
-		maxStraightPath(contour, vertex);
-		return null;
+	public static List<Point> straightPathPolygon(List<Point> contour, Point startVertex) {
+		List<Point> pivot = new ArrayList<Point>();
+		Point newVertex = startVertex;
+		do{
+			newVertex = maxStraightPath(contour, newVertex, startVertex);
+			pivot.add(newVertex);
+		} while(!(newVertex.x == startVertex.x && newVertex.y == startVertex.y));
+		return pivot;
 	}
 
-	public static List<Point> maxStraightPath(List<Point> contour, Point vertex){
+	public static Point maxStraightPath(List<Point> contour, Point vertex, Point startVertex){
 		Point c0 = new Point(0,0);
 		Point c1 = new Point(0,0);
 		
-		List<Point> pivot = new ArrayList<Point>(); 
 		int index = contour.indexOf(vertex);
 		while (true) {
 			Point vertex2Check = contour.get((index+1) % contour.size());
 			//checkDirections break
 			// calculate new vector
+			if (vertex2Check.x == startVertex.x && vertex2Check.y == startVertex.y) break;
 			if (constraintsViolated(vertex2Check, c0,c1)) {
 				break;
 			}
 			updateConstraints(vertex2Check, c0,c1);
 			index++;
 		}
-		pivot.add(contour.get(index+1));
-		return pivot;
+		return contour.get(index+1);
 	}
 	
 	private static boolean constraintsViolated(Point a, Point c0, Point c1){
