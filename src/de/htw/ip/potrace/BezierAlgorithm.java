fbac7cefcb11740ee765285aaa90e89f815296b6
Petulant Slacker <l.goohsen@gmail.com>
2016-12-29 20:20:28 +0100

    fixed beziercurves - alpha is still broken

--- src/de/htw/ip/potrace/BezierAlgorithm.java
+++ src/de/htw/ip/potrace/BezierAlgorithm.java
@@ -15,6 +15,7 @@ public class BezierAlgorithm {
 		polygons.forEach(polygon -> {
 			bezierCurves.add(generateBezierCurve(polygon, alphaMin, alphaMax, distanceFactor));
 		});
+//		bezierCurves.add(generateBezierCurve(polygons.get(1), alphaMin, alphaMax, distanceFactor));
 		bezierCurves.forEach(curve -> {
 			curve.forEach(elem -> System.out.println(elem.toString()));
 		});
@@ -23,31 +24,32 @@ public class BezierAlgorithm {
 	
 	private static List<CurveElement> generateBezierCurve(List<Point> polygon, double alphaMin, double alphaMax, double distanceFactor){
 		List<CurveElement> curve = new ArrayList<CurveElement>();
-		for (int i = 0; i < polygon.size(); i++) {
+		for (int i = 0; i < polygon.size(); i++) {//
 			Point2D.Double a = new Point2D.Double(polygon.get(i).x, polygon.get(i).y);
 			Point2D.Double midPoint1 = new Point2D.Double(
-					polygon.get((i-1 + polygon.size()) % polygon.size()).x - a.x/2,
-					polygon.get((i-1 + polygon.size()) % polygon.size()).y - a.y/2); 
+					a.x + (polygon.get((i-1 + polygon.size()) % polygon.size()).x - a.x)/2,
+					a.y + (polygon.get((i-1 + polygon.size()) % polygon.size()).y - a.y)/2); 
 			Point2D.Double midPoint2 = new Point2D.Double(
-					polygon.get((i+1) % polygon.size()).x - a.x/2,
-					polygon.get((i+1) % polygon.size()).y - a.y/2);
+					a.x + (polygon.get((i+1) % polygon.size()).x - a.x)/2,
+					a.y + (polygon.get((i+1) % polygon.size()).y - a.y)/2);
 			Point2D.Double s_ = new Point2D.Double(midPoint2.y - midPoint1.y, -midPoint2.x + midPoint1.x);
 			double s_length = Math.sqrt(Math.pow(s_.x, 2) + Math.pow(s_.y, 2));
 			Point2D.Double s = new Point2D.Double(s_.x/s_length, s_.y/s_length );
 			double d = scalarProduct(s, new Point2D.Double(a.x - midPoint1.x, a.y - midPoint1.y));
-			double alpha = distanceFactor * (d - 0.5)/d; 
+			double alpha = (4/3) * (d - 0.5)/d; //distanceFactor 
 			alpha = alpha < alphaMin? alphaMin: alpha;
 			
-			if (alpha > alphaMax){
+			if (alpha < alphaMax){
 				curve.add(new LineElement(a, midPoint2));
 			} else {
+				alpha = 0.7;
 				Point2D.Double z1 = new Point2D.Double(
-						alpha * (a.x - midPoint1.x),
-						alpha * (a.y - midPoint1.y));
+						a.x + alpha * (midPoint1.x - a.x),
+						a.y + alpha * (midPoint1.y - a.y));
 				
 				Point2D.Double z2 = new Point2D.Double(
-						alpha * (a.x - midPoint2.x),
-						alpha * (a.y - midPoint2.y));
+						a.x + alpha * (midPoint2.x - a.x),
+						a.y + alpha * (midPoint2.y - a.y));
 				curve.add(new BezierElement(z1, z2, midPoint2));
 			}
 		}
